59b53e91c2a5877c3d95ebf7eec901b8d2b99725
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2018-04-05 13:54:30 +0200

    Pythranising sources

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 # encoding: UTF-8
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, dist
 
+# Declare the dependency
+dist.Distribution(dict(setup_requires='pythran'))
+
+# Pythran modules
+from pythran import PythranExtension
+
+module_parse = PythranExtension('gaussdca._load_data', sources=['gaussdca/_load_data.py'])
+module_gdca = PythranExtension('gaussdca._gdca', sources=['gaussdca/_gdca.py'])
+
+
+# Main setup:
 setup(name='pyGaussDCA', version='0.1',
       description='',
       url='',
@@ -12,6 +23,7 @@ setup(name='pyGaussDCA', version='0.1',
       packages=find_packages(),
       package_data={'gaussdca.data': ['gaussdca/test/data/*.a3m']},
       include_package_data=True,
+      ext_modules=[module_parse, module_gdca],
       requires=['numpy', 'scipy', 'pythran'],
       classifiers=['Programming Language :: Python',
                    'Topic :: Scientific/Engineering :: Bio-Informatics',
