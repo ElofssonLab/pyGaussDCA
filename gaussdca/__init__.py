70e0c29f2abc29be7e8259ee47191d847a030f96
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2018-04-05 13:59:39 +0200

    Include warning if not pythranised

--- gaussdca/__init__.py
+++ gaussdca/__init__.py
@@ -1,3 +1,10 @@
 from __future__ import absolute_import
 
+from . import _load_data
+from . import _gdca
+
+if not hasattr(_load_data, '__pythran__') or not hasattr(_gdca, '__pythran__'):
+    import warnings
+    warnings.warn(RuntimeWarning('Modules were not compiled, pyGaussDCA will be slow.'))
+
 from .gaussdca import run
