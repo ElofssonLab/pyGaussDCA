e4296ac6ac3aa4f28d6fd4e8c24600e3463cf245
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2018-04-05 17:30:42 +0200

    Tidying up __init__

--- gaussdca/__init__.py
+++ gaussdca/__init__.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import
 
 from . import _load_data
 from . import _gdca
+from .gaussdca import run
 
 if not hasattr(_load_data, '__pythran__') or not hasattr(_gdca, '__pythran__'):
     import warnings
-    warnings.warn(RuntimeWarning('Modules were not compiled, pyGaussDCA will be slow.'))
 
-from .gaussdca import run
+    warnings.warn(RuntimeWarning('Modules were not compiled, pyGaussDCA will be slow.'))
