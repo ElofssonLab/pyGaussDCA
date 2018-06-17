eae8a3273d2da7b8aa1d761b313ec615895b74f9
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2018-06-17 17:34:03 +0200

    Add link opemp flags

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ from pythran import PythranExtension
 
 module_parse = PythranExtension('gaussdca._load_data', sources=['gaussdca/_load_data.py'])
 module_gdca = PythranExtension('gaussdca._gdca', sources=['gaussdca/_gdca.py'],
-                               extra_compile_args=['-fopenmp', '-ftree-vectorize'])
+                               extra_compile_args=['-fopenmp', '-ftree-vectorize'],
+                               extra_link_args=['-fopenmp'])
 
 # Main setup:
 setup(name='pyGaussDCA', version='0.1.1',
