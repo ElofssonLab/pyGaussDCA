f1b997619a51a4ffbe2fd752b0acf40e59e345b7
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2019-01-30 18:49:19 +0100

    Re-structure tests to eat data

---
+++ tests/run_tests.sh
@@ -0,0 +1,11 @@
+
+if [ -r /usr/lib/libeatmydata/libeatmydata.so ]; then
+  # much faster package installation
+  export LD_PRELOAD='/usr/lib/libeatmydata/libeatmydata.so'
+elif [ -r /usr/lib/*/libeatmydata.so ]; then
+  # much faster package installation
+  export LD_PRELOAD='/usr/$LIB/libeatmydata.so'
+fi
+
+py.test
+
